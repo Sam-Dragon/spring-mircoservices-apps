003614738575f3a841af901a731cd9390fe1b385
Sam-Dragon <rahulsheth2013@gmail.com>
2026-01-24 16:15:59 +0530

    # Master
    - httpexchange endpoint bean

--- user-service/src/main/java/com/learning/microservices/UserServiceApplication.java
+++ user-service/src/main/java/com/learning/microservices/UserServiceApplication.java
@@ -1,12 +1,20 @@
 package com.learning.microservices;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Bean
+    public HttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
     }
 }
